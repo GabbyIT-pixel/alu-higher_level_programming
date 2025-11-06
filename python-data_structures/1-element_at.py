16b6b295647ce4a492c661e5d78d18d6f855ba8c
Gabriel Mugisha <g.mugisha4@alustudent.com>
2025-11-06 16:30:04 +0200

    The Second Question

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Return element at a given index like in C.
+"""
+
+def element_at(my_list, idx):
+    """Retrieve element at index `idx` from `my_list`.
+
+    Returns None when idx is negative or out of range.
+    """
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
